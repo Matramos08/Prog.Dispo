091d15478224c6c0fc9eeae51a28a2960293f992
Matramos08 <ramosmatheus92@gmail.com>
2026-07-16 16:08:21 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+import { ref } from "vue";
+const id = ref(2);
+const textoParaAdicionar = ref("");
+const headers = ref([{id: 1, texto: "conseguiu!"}])
+
+function adicionar() {
+if (!textoParaAdicionar.value) return;
+headers.value.push({id: id.value, texto: textoParaAdicionar.value});
+id.value++;
+textoParaAdicionar.value = "";
+}
+</script>
+
+<template>
+<header>
+<img
+alt="Vue logo"
+class="logo"
+src="./assets/logo.svg"
+width="125"
+height="125"
+/>
+
+<div class="wrapper">
+<HelloWorld v-for="mensagem in headers" :msg="mensagem.texto" :key="mensagem.id"/>
+</div>
+</header>
+
+<main>
+<TheWelcome />
+<input v-model="textoParaAdicionar">
+<button @click="adicionar">criar texto</button>
+</main>
+</template>
+
+<style scoped>
+header {
+line-height: 1.5;
+}
+
+.logo {
+display: block;
+margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+header {
+display: flex;
+place-items: center;
+padding-right: calc(var(--section-gap) / 2);
+}
+
+.logo {
+margin: 0 2rem 0 0;
+}
+
+header .wrapper {
+display: flex;
+place-items: flex-start;
+flex-wrap: wrap;
+}
+}
+</style>
